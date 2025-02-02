e31855004bc8a1509ee003859e69e4a2ae16f70e
Maxim Davidson <maxd02122004@gmail.com>
2025-02-02 14:11:16 +0300

    feat(LR-03): Added localization

--- src/ui/RegisterModal/registerModal.tsx
+++ src/ui/RegisterModal/registerModal.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import style from './registerModal.module.scss'
 import googleLogo from '../../assets/googleLogo.png'
 import { loginWithEmail, loginWithGoogle, registrationWithEmail } from '../../service/authService'
+import { useTranslation } from 'react-i18next'
 
 interface RegisterModalProps {
   closeModal: () => void
@@ -12,6 +13,8 @@ export const RegisterModal = ({ closeModal }: RegisterModalProps) => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const { t } = useTranslation()
 
   const handleSetName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value)
@@ -42,30 +45,46 @@ export const RegisterModal = ({ closeModal }: RegisterModalProps) => {
         <button className={style.close_button} onClick={closeModal}>
           ✖
         </button>
-        <h2>{isLoginMode ? 'Вход' : 'Регистрация'}</h2>
+        <h2>{t(isLoginMode ? 'registerModal.login' : 'registerModal.registration')}</h2>
 
         {!isLoginMode && (
-          <input type='text' placeholder='Имя' value={name} onChange={handleSetName} />
+          <input
+            type='text'
+            placeholder={t('registerModal.name')}
+            value={name}
+            onChange={handleSetName}
+          />
         )}
 
-        <input type='email' placeholder='Email' value={email} onChange={handleSetEmail} />
-        <input type='password' placeholder='Пароль' value={password} onChange={handleSetPassword} />
+        <input
+          type='email'
+          placeholder={t('registerModal.email')}
+          value={email}
+          onChange={handleSetEmail}
+        />
+        <input
+          type='password'
+          placeholder={t('registerModal.password')}
+          value={password}
+          onChange={handleSetPassword}
+        />
 
         <button className={`${style.button} ${style.register}`} onClick={handleSubmit}>
-          {isLoginMode ? 'Войти' : 'Зарегистрироваться'}
+          {t(isLoginMode ? 'registerModal.submitLogin' : 'registerModal.submitRegister')}
         </button>
 
         <div className={style.switch_mode}>
-          {isLoginMode ? 'Нет аккаунта? ' : 'Уже есть аккаунт? '}
+          {isLoginMode ? t('registerModal.noAccount') : t('registerModal.haveAccount')}
           <a className={style.mode_toggle} onClick={() => setIsLoginMode(!isLoginMode)}>
-            {isLoginMode ? 'Зарегистрироваться' : 'Войти'}
+            {isLoginMode ? t('registerModal.switchToRegister') : t('registerModal.switchToLogin')}
           </a>
         </div>
 
-        <p>или</p>
+        <p>{t('registerModal.or')}</p>
 
         <button className={`${style.button} ${style.google}`} onClick={loginWithGoogle}>
-          <img src={googleLogo} alt='Google' /> Продолжить с Google
+          <img src={googleLogo} alt='Google' />
+          {t('registerModal.continueWithGoogle')}
         </button>
       </div>
     </div>
